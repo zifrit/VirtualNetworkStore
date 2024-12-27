c040cdf8d61738ed639bf5b06cb0cbfe2adffcca
zifrit <zifrit@list.ru>
2024-12-27 12:22:25 +0300

    добавил обратную связь с заказами и пользовательскими виртуальными сетями

--- src/models/user.py
+++ src/models/user.py
@@ -1,13 +1,19 @@
+import typing
+
 from src.models.base import IdCUDMixin
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 from sqlalchemy import String, BigInteger, ForeignKey
+
+if typing.TYPE_CHECKING:
+    from src.models.vpn import UserVPNs
+    from src.models.order import Order
 
 
 class TgUser(IdCUDMixin):
     __tablename__ = "tg_users"
     username: Mapped[str] = mapped_column(String(255))
     tg_id: Mapped[int] = mapped_column(BigInteger, unique=True)
-    # user_vpn_s: Mapped[list["UserVPNs"]] = relationship(back_populates="tg_user")
+    user_vpn_s: Mapped[list["UserVPNs"]] = relationship(back_populates="tg_user")
     # # Рефералы, которых пригласил данный пользователь
     referred_users: Mapped[list["Referral"]] = relationship(
         "Referral",
@@ -20,7 +26,7 @@ class TgUser(IdCUDMixin):
         back_populates="referred_user",
         foreign_keys="[Referral.referred_user_id]",
     )
-    # orders: Mapped[list["Order"]] = relationship(back_populates="tg_user")
+    orders: Mapped[list["Order"]] = relationship(back_populates="tg_user")
 
     repr_columns = ["tg_id", "username"]
 
